376bb77d84e924fa6699f530462a5af4d027814d
Joel Ferrier <joel@ferrier.io>
2017-02-11 10:43:09 -0800

    unit test for cli.parse_args

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,17 +4,79 @@ import pytest
 import boto3
 import base64
 import os
+import sys
+import copy
 
 from faker import Factory
-from dotenv import Dotenv
-from aws_ir.aws_ir import cli
 
-def setup_module():
-    """To-do figure out how to properly test arg parser"""
-    pass
+sys.path.append(os.getcwd()+"/aws_ir")
+from ..aws_ir import cli
 
-def test_command_line_exists():
-    pass
+
+@pytest.fixture
+def cli_object():
+    cli_object = cli.cli()
+    return cli_object
+
+def test_parse_args():
+    print(sys.path)
+    #assert(False)
+    cli = cli_object()
+    case_number = "cr-17-000001-2d2d"
+    cidr_range = "0.0.0.0/0"
+    bucket_name = "crn-00001-assets"
+    optional_args = ["--case-number", case_number,
+                     "--examiner-cidr-range", cidr_range,
+                     "--bucket-name", bucket_name, "--dry-run"]
+    instance_ip = "172.16.20.1"
+    user = "ec2-user"
+    ssh_key = "ssh.key"
+    host_compromise_args = ["host-compromise", "--instance-ip", instance_ip,
+                            "--user", user, "--ssh-key", ssh_key]
+    access_key_id = "AKIAIOSFODNN7EXAMPLE"
+    key_compromise_args = ["key-compromise", "--access-key-id",
+                           access_key_id]
+
+    host_args = copy.copy(optional_args)
+    key_args = copy.copy(optional_args)
+
+    host_args.extend(host_compromise_args)
+    parsed_host_args = cli.parse_args(host_args)
+
+    key_args.extend(key_compromise_args)
+    parsed_key_args = cli.parse_args(key_args)
+
+    # Check Host compromise arguments
+    assert parsed_host_args.case_number == case_number
+    assert parsed_host_args.examiner_cidr_range == cidr_range
+    assert parsed_host_args.bucket_name == bucket_name
+    assert parsed_host_args.dry_run == True
+    # Check Host compromise required arguments
+    assert parsed_host_args.instance_ip == instance_ip
+    assert parsed_host_args.user == user
+    assert parsed_host_args.ssh_key == ssh_key
+    # Check Key Compromise required arguments are not present
+    with pytest.raises(AttributeError):
+        assert parsed_host_args.access_key_id == access_key_id
+    # Check that the correct fuction has been selected
+    assert parsed_host_args.func == "host_compromise"
+
+    # Check key compromise optional arguments
+    assert parsed_key_args.case_number == case_number
+    assert parsed_key_args.examiner_cidr_range == cidr_range
+    assert parsed_key_args.bucket_name == bucket_name
+    assert parsed_key_args.dry_run == True
+    # Check key compromise required arguments
+    assert parsed_key_args.access_key_id == access_key_id
+    # Check Host Compromise require arguments are not present
+    with pytest.raises(AttributeError):
+        assert parsed_key_args.instance_ip == instance_ip
+    with pytest.raises(AttributeError):
+        assert parsed_key_args.user == user
+    with pytest.raises(AttributeError):
+        assert parsed_key_args.ssh_key == ssh_key
+    # Check that the correct fuction has been selected
+    assert parsed_key_args.func == "key_compromise"
 
 
 def teardown_module():
