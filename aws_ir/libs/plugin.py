89d1f66230a9905f3b90c5ec4ef389dd0e38cb0d
Andrew J Krug <andrewkrug@gmail.com>
2017-06-04 15:43:38 -0700

    Feature/plugin loading (#49)
    * update travis to stub config
    
    * support plugin loading for core and community
    
    * update travis file
    
    * update travis file
    
    * update travis file
    
    * update travis file
    
    * update travis file
    
    * pep8 fixes

---
+++ aws_ir/libs/plugin.py
@@ -0,0 +1,37 @@
+import os
+
+from functools import partial
+from pluginbase import PluginBase
+
+
+class Core(object):
+    """Enumerates core plugins that are part of the AWS_IR offering."""
+    def __init__(self):
+        self.here = os.path.abspath(os.path.dirname(__file__))
+        get_path = partial(os.path.join, self.here)
+
+        self.plugin_base = PluginBase(
+            package='aws_ir.plugins',
+            searchpath=[get_path('../plugins')]
+        )
+
+        self.source = self.plugin_base.make_plugin_source(
+            searchpath=[get_path('../plugins'), get_path('~/.awsir/plugins')]
+        )
+
+        self.list = self.source.list_plugins()
+
+
+class Custom(object):
+    """Enumerates core plugins that are part of the AWS_IR offering."""
+    def __init__(self):
+        self.plugin_base = PluginBase(
+            package='aws_ir.plugins',
+            searchpath=[(os.getenv("HOME") + '/.awsir/plugins')]
+        )
+
+        self.source = self.plugin_base.make_plugin_source(
+            searchpath=[(os.getenv("HOME") + '/.awsir/plugins')]
+        )
+
+        self.list = self.source.list_plugins()
