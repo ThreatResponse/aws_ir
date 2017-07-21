b1cb8e884b0234da567e94b84af3196a998bf9fb
andrewkrug <andrewkrug@gmail.com>
2017-07-21 07:53:15 -0700

    fix test rename method reference

--- tests/test_case_lib.py
+++ tests/test_case_lib.py
@@ -143,7 +143,7 @@ class TestCaseLib(object):
             f.write('test jpg data')
             f.close()
 
-        result = self.generic_case._Case__rename_log_file(
+        result = self.generic_case._rename_log_file(
             self.generic_case.case_number,
             'i-12345678',
             base_dir=self.log_base
@@ -214,7 +214,7 @@ class TestCaseLib(object):
                 f.write('test jpg data')
                 f.close()
 
-            self.generic_case._Case__rename_log_file(
+            result = self.generic_case._rename_log_file(
                 self.generic_case.case_number,
                 'i-12345678',
                 base_dir=self.log_base
